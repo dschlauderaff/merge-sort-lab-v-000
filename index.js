c908851bfe5ce1f37e53128b464bdd5556a9322c
David Schlauderaff <dschlaud@gmail.com>
2018-01-28 17:11:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function findMinAndRemoveSorted(array) {
-  mergeSort(array)
+  // mergeSort(array)
   array.shift(0)
   
 }
@@ -10,4 +10,12 @@ function mergeSort(array) {
   let firstHalf = array.slice(0, midpoint)
   let secondHalf = array.slice(midpoint, array.length)
   return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+}
+
+function merge(firstHalf, secondHalf) {
+  let sorted = []
+  let currentMin
+  while(firstHalf.length != 0 && secondHalf.length != 0) {
+    let currentMin 
+  }
 }
